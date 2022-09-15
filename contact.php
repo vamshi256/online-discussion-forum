22910f4ef20cd780f7a4eb7d871a27dcd1137cd8
vamshi256 <75255882+vamshi256@users.noreply.github.com>
2022-09-15 09:35:56 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php  require("header.php"); ?>
+<script type="text/javascript">
+	document.getElementById("acontact").className="active";
+</script>
+
+
+	<h1>Contact Us</h1>
+    <table>
+    
+	
+    <tr><td rowspan="4"><img src="ups/satish.jpg" style='height:100px; width:100px';></td><td>Name</td><td>:</td><td>tech_grp</td></tr>
+    <tr><td rowspan="1">Contact No</td><td>:</td><td>00678016</td></tr>
+    <tr><td rowspan="1">E-Mail Add</td><td>:</td><td>phptpoint@gmail.com</td></tr>
+   <tr><td rowspan="1">Website</td><td>:</td><td><a href="">www.tech_grp.com</a></td></tr>
+    </table>
+    
+<?php require("footer.php"); ?>
